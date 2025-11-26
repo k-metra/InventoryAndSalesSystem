bb75586fede6b98c3ff4ab018eed4bc4e6da3277
k-metra <metrakurt@gmail.com>
2025-11-26 21:05:57 +0800

    initial commit

---
+++ frontend/axios/api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const localApi = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+})
+
+const api = localApi;
+
+export default api;
+// TODO: create localApi and productionApi based on environment variables and set api accordingly
