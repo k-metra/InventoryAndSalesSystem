0a232abfd17a7196010ce53324a4ba0c9dd51212
k-metra <metrakurt@gmail.com>
2025-11-26 22:12:15 +0800

    initialize login function for AuthController

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'username' => 'required|string|max:40',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+
+        if (! Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid username or password.'], 401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'user' => Auth::user(),
+        ]);
+    }
+}
