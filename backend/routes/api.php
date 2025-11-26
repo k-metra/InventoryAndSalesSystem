4e6a7d482eb43012dc8e17a5f168f14a5e043666
k-metra <metrakurt@gmail.com>
2025-11-26 22:13:53 +0800

    wrap login route in web guard middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['web'])->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
