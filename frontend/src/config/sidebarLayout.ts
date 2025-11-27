7cb2c7afb90bfc41963d436b1355ae2bf5bd721e
k-metra <metrakurt@gmail.com>
2025-11-27 21:57:45 +0800

    implement sidebar layout config file containnig the href link for each sidebar item

---
+++ frontend/src/config/sidebarLayout.ts
@@ -0,0 +1,11 @@
+const sidebarLayout = {
+    'Dashboard': '/dashboard',
+    'Products': '/dashboard/products',
+    'Inventory': '/dashboard/inventory',
+    'Sales': '/dashboard/sales',
+    'Customers': '/dashboard/customers',
+    'Suppliers': '/dashboard/suppliers',
+    'Settings': '/dashboard/settings'
+}
+
+export default sidebarLayout;
