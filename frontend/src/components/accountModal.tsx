627f0f7f9df7a2c6dc4d72b3a8da3dc53b23ac54
k-metra <metrakurt@gmail.com>
2025-11-27 21:02:59 +0800

    feat(account modal): display role if available

--- frontend/src/components/accountModal.tsx
+++ frontend/src/components/accountModal.tsx
@@ -42,9 +42,10 @@ export default function AccountModal({ showModal, onClose }: { showModal: boolea
         <div ref={modalRef} className={`absolute right-2 origin-top top-12 ${!showModal ? 'opacity-0 scale-y-0 scale-x-0 pointer-events-none' : 'opacity-100 scale-x-100 scale-y-100 pointer-events-auto'} p-3 flex flex-col gap-0 bg-secondary border border-black/25 rounded-md shadow-lg min-w-50 transition-transform-opacity duration-300 ease-out`}>
             <div className={`flex flex-row w-full items-center justify-between ${showModal ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300 ease-out`}>
                 {user !== null ? <FaUser size={40} className="text-text ml-3" /> : <FaUserAltSlash size={40} className="text-text/60 ml-3" /> }
-                <div className="self-end">
-                    <span className="text-muted text-[12px] text-right m-0">Logged in as</span>
+                <div className="flex flex-col justify-end self-end pb-4">
+                    <span className="text-muted text-[11px] text-right m-0">Logged in as</span>
                     <h5 className="font-bold text-text text-right">{displayName}</h5>
+                    {user?.role && <span className=" text-muted text-[12px] font-semibold text-right m-0 w-full">{user.role}</span> }
                 </div>
             </div>
             <hr className="border-t border-black/25 mt-2 mb-4" />
