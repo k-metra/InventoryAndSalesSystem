361a7b960a0af4c7ebe9a8371fc11c8017ba9bd4
k-metra <metrakurt@gmail.com>
2025-11-27 20:38:22 +0800

    add logout route with auth:sanctum middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,6 +9,8 @@ Route::middleware(['web'])->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
 
+Route::middleware(['auth:sanctum'])->post('/logout', [AuthController::class, 'logout']);
+
 Route::middleware(['auth:sanctum'])->get('/me', function (Request $request) {
     return response()->json([
         'user' => auth()->user(),
