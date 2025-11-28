b648150e6e389bc30662b08113735a9eb932f05d
k-metra <metrakurt@gmail.com>
2025-11-28 16:34:12 +0800

    add count method to api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,6 +21,7 @@ Route::middleware(['auth:sanctum'])->get('/me', function (Request $request) {
 // Products
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/all', [ProductController::class, 'count']); // Move this before {id} route
     Route::post('/products', [ProductController::class, 'store']);
     Route::get('/products/{id}', [ProductController::class, 'show']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
