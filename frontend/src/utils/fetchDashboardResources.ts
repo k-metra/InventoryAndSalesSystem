6709019c17bfa27504e6c59682c949eb001dfd9c
k-metra <metrakurt@gmail.com>
2025-11-28 17:12:17 +0800

    refactor: update fetchDashboardResources to return structured data and format inventory value

--- frontend/src/utils/fetchDashboardResources.ts
+++ frontend/src/utils/fetchDashboardResources.ts
@@ -1,11 +1,12 @@
 import api from "./../axios/api";
+import formatCurrency from "./formatCurrency";
 
-type dashboardResources = Record<string, number>;
+type dashboardResources = Record<string, number | string>;
 
 const fetchDashboardResources = new Promise<dashboardResources>(async (resolve, _) => {
-    const productCount: number = 
+    const { totalProducts, totalInventoryValue } = 
         await api.get('/products/all')
-        .then(res => res.data.count)
+        .then(res => res.data)
         .catch((err) => {
             console.log("Error fetching product count", err);
             return 0;
@@ -13,8 +14,8 @@ const fetchDashboardResources = new Promise<dashboardResources>(async (resolve, 
 
     resolve(
         {
-            productCount: productCount,
-            totalInventoryValue: 7500,
+            productCount: totalProducts,
+            totalInventoryValue: formatCurrency(totalInventoryValue),
             totalSales: 127,
             totalCustomers: 12
         }
