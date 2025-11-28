5c86ed7f952de6136ee8ff43218096e20a854b63
k-metra <metrakurt@gmail.com>
2025-11-28 16:33:31 +0800

    disable timestamps for product

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
+
+    public $timestamps = false;
+
     protected $fillable = [
         'name',
         'description',
