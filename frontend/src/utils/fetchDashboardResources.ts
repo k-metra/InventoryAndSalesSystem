25347c5f946f806aae2edf35e9dd6f12888940b0
k-metra <metrakurt@gmail.com>
2025-11-29 13:23:33 +0800

    refactor(fetchDashboardResources): simplify function implementation and improve error handling

--- frontend/src/utils/fetchDashboardResources.ts
+++ frontend/src/utils/fetchDashboardResources.ts
@@ -3,23 +3,47 @@ import formatCurrency from "./formatCurrency";
 
 type dashboardResources = Record<string, number | string>;
 
-const fetchDashboardResources = new Promise<dashboardResources>(async (resolve, _) => {
-    const { totalProducts, totalInventoryValue } = 
-        await api.get('/products/all')
-        .then(res => res.data)
-        .catch((err) => {
-            console.log("Error fetching product count", err);
-            return 0;
-        });
+const fetchDashboardResources = async () => {
+    console.log("Fetching dashboard resources...");
 
-    resolve(
-        {
-            productCount: totalProducts,
-            totalInventoryValue: formatCurrency(totalInventoryValue),
-            totalSales: 127,
-            totalCustomers: 12
-        }
-    );
-})
+    const { totalProducts, 
+        totalInventoryValue } = await api.get('/products/all')
+    .then(res => res.data)
+    .catch((err) => {
+        console.log("Error fetching product count", err);
+        return {
+            totalProducts: 0,
+            totalInventoryValue: 0
+        };
+    });
+
+    return {
+        productCount: totalProducts,
+        totalInventoryValue: formatCurrency(totalInventoryValue),
+        totalSales: 127,
+        totalCustomers: 12
+    };
+}
+
+
+// const fetchDashboardResources = new Promise<dashboardResources>(async (resolve, _) => {
+//     console.log("Fetching dashboard resources...");
+//     const { totalProducts, totalInventoryValue } = 
+//         await api.get('/products/all')
+//         .then(res => res.data)
+//         .catch((err) => {
+//             console.log("Error fetching product count", err);
+//             return 0;
+//         });
+
+//     resolve(
+//         {
+//             productCount: totalProducts,
+//             totalInventoryValue: formatCurrency(totalInventoryValue),
+//             totalSales: 127,
+//             totalCustomers: 12
+//         }
+//     );
+// });
 
 export default fetchDashboardResources;
