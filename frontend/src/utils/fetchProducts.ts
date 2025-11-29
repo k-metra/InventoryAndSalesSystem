15b98fd9dee4e3e409c6c2918c1bd78c56dac42b
k-metra <metrakurt@gmail.com>
2025-11-29 17:42:26 +0800

    refactor: fetchProducts return actual data instead of a promise

--- frontend/src/utils/fetchProducts.ts
+++ frontend/src/utils/fetchProducts.ts
@@ -1,15 +1,26 @@
-import api from './../axios/api';
+import api from './../axios/api'; 
+
+export type Product = {
+    id: number;
+    name: string;
+    sku: string;
+    category?: { id: number; name: string; };
+    supplier?: { id: number; name: string; };
+    price: number;
+    stock: number;
+}
 
 const fetchProducts = async () => {
     console.log("Fetching products...");
 
-    const products = api.get('/products')
-    .then(res => res.data)
+    const products = await api.get('/products')
+    .then(res => res.data.products)
     .catch((err) => {
         console.log("Error fetching products", err);
         return [];
     });
 
+    console.log(products);
     return products;
 }
 
