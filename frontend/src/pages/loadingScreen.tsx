d42724555ff5b03990db3a8fb94615122a5498d3
k-metra <metrakurt@gmail.com>
2025-11-29 13:22:46 +0800

    feat(loading screen): add component prop for full screen

--- frontend/src/pages/loadingScreen.tsx
+++ frontend/src/pages/loadingScreen.tsx
@@ -1,8 +1,8 @@
 import { ImSpinner2 } from "react-icons/im";
 
-export default function LoadingScreen() {
+export default function LoadingScreen({ fullscreen }: { fullscreen?: boolean }) {
     return (
-        <div className="w-screen h-screen flex justify-center items-center bg-background">
+        <div className={`flex justify-center items-center ${fullscreen ? "w-screen h-screen" : "w-full h-full"}`}>
             <ImSpinner2 size={50} className="text-primary animate-spin" />
         </div>
     )
