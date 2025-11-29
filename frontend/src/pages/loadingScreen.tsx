aa3fed5a963ea98a10a30f1798a762cd47733a2f
k-metra <metrakurt@gmail.com>
2025-11-29 14:09:02 +0800

    refactor(loadingScreen): remove fullscreen prop and simplify layout

--- frontend/src/pages/loadingScreen.tsx
+++ frontend/src/pages/loadingScreen.tsx
@@ -1,8 +1,8 @@
 import { ImSpinner2 } from "react-icons/im";
 
-export default function LoadingScreen({ fullscreen }: { fullscreen?: boolean }) {
+export default function LoadingScreen() {
     return (
-        <div className={`flex justify-center items-center ${fullscreen ? "w-screen h-screen" : "w-full h-full"}`}>
+        <div className={`flex justify-center items-center w-full h-full min-h-screen max-w-screen`}>
             <ImSpinner2 size={50} className="text-primary animate-spin" />
         </div>
     )
