7cc34e8862f2f9876b14416081a84e331422fb7f
k-metra <metrakurt@gmail.com>
2025-11-30 13:08:45 +0800

    fix: return all suppliers as a json response

--- backend/app/Http/Controllers/SupplierController.php
+++ backend/app/Http/Controllers/SupplierController.php
@@ -12,7 +12,7 @@ class SupplierController extends Controller
      */
     public function index()
     {
-        return Supplier::all();
+        return response()->json(Supplier::all());
     }
 
     /**
