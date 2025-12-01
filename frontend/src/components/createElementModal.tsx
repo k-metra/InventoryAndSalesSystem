d752c692b89c8010437078acee627625369b6a9b
k-metra <metrakurt@gmail.com>
2025-12-01 21:48:24 +0800

    feat: create reusable create element modal component

---
+++ frontend/src/components/createElementModal.tsx
@@ -0,0 +1,154 @@
+import { useQueries, useQueryClient, useMutation } from '@tanstack/react-query';
+import api from '../axios/api';
+import { type Field, type OptionField } from '../types/fields';
+
+import InputField from './inputField';
+
+import { useState, type ChangeEvent } from 'react';
+import getNestedValue from '../utils/getNestedValue';
+import setNestedValue from '../utils/setNestedValue';
+
+import { ImSpinner7 } from "react-icons/im";
+import { IoMdClose } from "react-icons/io";
+import { FaPencil } from "react-icons/fa6";
+import { useToast } from '../contexts/ToastContext';
+
+type CreateElementModalProps = {
+    application: string;
+    fields: Field[];
+    onClose: () => void;
+}
+
+
+export default function CreateElementModal({application, fields, onClose }: CreateElementModalProps) {
+    const { addToast } = useToast();
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: (newData: Record<string, any>) => {
+            return api.post(`/${application}`, newData).then(res=>res.data);
+        },
+
+        onSuccess: () => {
+            addToast(`${application.charAt(0).toUpperCase() + application.slice(1)} created successfully.`, 'success', 5000);
+            queryClient.invalidateQueries({ queryKey: [application]})
+            onClose();
+        },
+
+        onError: (error) => {
+            addToast(`Ran into an error creating ${application}. Please try again.`, 'error', 10000);
+            console.error(error);
+        }
+    })
+
+    const optionsQueries = useQueries({
+        queries: fields
+        .filter((field): field is OptionField => field.type === 'options' && !!field.fetchOptions)
+        .map((field) => ({
+            queryKey: [`${application}`, field.key, 'options'],
+            queryFn: field.fetchOptions!
+        }))
+    });
+
+    const optionsMap = fields
+        .filter((field): field is OptionField => field.type === 'options' && !!field.fetchOptions)
+        .reduce((accumulator, field, index) => {
+            accumulator[field.key] = optionsQueries[index].data || [];
+            return accumulator;
+        }, {} as Record<string, any[]>);
+
+
+    const [formState, setFormState] = useState<Record<string, any>>({});
+
+
+
+    const handleChange = (key: string, value: any) => {
+        setFormState(prev => {
+            const copy = { ... prev };
+            setNestedValue(copy, key, value);
+
+            return copy;
+        })
+        ;
+    };
+
+    const handleSave = async () => {
+        mutation.mutate(formState);
+    }
+
+
+    return (
+        <div className="fixed h-screen w-screen bg-black/60 top-0 left-0 flex items-center justify-center z-10000">
+                    <div className="custom-scrollbar relative bg-white rounded-lg max-w-[70vh] max-h-[80vh] overflow-y-auto">
+                        <div className="p-6">
+                            <h5 className="font-bold text-text mb-4">Create {application.charAt(0).toUpperCase() + application.slice(1)}</h5>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                                 {fields.map((field) => {
+                            if (field.type === 'readonly') {
+                                return (
+                                    <div key={field.key} className="mb-4">
+                                        <label className="block text-sm font-medium text-gray-700 mb-1">{field.label}</label>
+                                        <p className="text-gray-900">{formState[field.key]}</p>
+                                    </div>
+                                );
+                            } else if (field.type === 'text') {
+                                return (
+                                    <div key={field.key} className="mb-4">
+                                        <label className="block text-sm font-medium text-gray-700 mb-1">{field.label}</label>
+                                        <InputField
+                                            type="text"
+                                            value={formState[field.key] || ''}
+                                            onChange={(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => handleChange(field.key, e.target.value)}
+                                        />
+                                       
+                                    </div>
+                                )
+                            } else if (field.type === 'options') {
+                                const options = optionsMap[field.key] || [];
+
+                                return (
+                                    <div key={field.key} className="mb-4">
+                                        <label className="block text-sm font-medium text-gray-700 mb-1">{field.label}</label>
+
+                                        <InputField
+                                            type="options"
+                                            data={options} 
+                                            value={getNestedValue(formState, field.key) || ''} 
+                                            onChange={(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => handleChange(field.key, e.target.value)}
+                                        />
+
+                                    </div>
+                                )
+                            } else if (field.type === 'number') {
+                                return (
+                                    <div key={field.key} className="mb-4">  
+                                        <label className="block text-sm font-medium text-gray-700 mb-1">{field.label}</label>   
+                                        <InputField
+                                            type="number"
+                                            value={formState[field.key] || ''}
+                                            onChange={(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => handleChange(field.key, e.target.value)}
+                                        />
+                                    </div>
+                                )
+                            }
+                        })}
+                            </div>
+                        </div>
+
+                        <div className="flex border-t border-black/25 justify-end items-center gap-2 bottom-0 left-0 sticky p-2 bg-white *:cursor-pointer">
+                            <button onClick={handleSave} disabled={mutation.isPending} className={`inline-flex items-center gap-2 bg-primary text-white px-4 py-2 disabled:opacity-50 rounded`}>
+                                {mutation.isPending && <ImSpinner7 size={20} className="inline-block text-white animate-spin" />}
+                                Create
+                                <FaPencil className="inline-block text-white" size={15} />
+                            </button>
+                            <button onClick={onClose} disabled={mutation.isPending} className="inline-flex items-center gap-2 bg-red-500 disabled:opacity-50 text-white px-4 py-2 rounded">
+                                Cancel
+                                <IoMdClose className="inline-block text-white" size={24} />
+                            </button>
+                        </div>
+                    </div>
+     
+        </div>
+    )
+}
