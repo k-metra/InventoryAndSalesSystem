45c3fb8f0f6b7623b9412722468f188b24e82773
k-metra <metrakurt@gmail.com>
2025-12-03 19:21:45 +0800

    fix(Category): add product relationship to category model

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 
 class Category extends Model
 {
@@ -16,4 +17,8 @@ class Category extends Model
     ];
 
     public $timestamps = false;
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
