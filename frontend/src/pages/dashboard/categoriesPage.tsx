13dc333a0985945fbca7513804b7a102dbb70f2a
k-metra <metrakurt@gmail.com>
2025-12-03 18:46:20 +0800

    initialize categories page

---
+++ frontend/src/pages/dashboard/categoriesPage.tsx
@@ -0,0 +1,7 @@
+export default function CategoriesPage() {
+    return (
+        <div className="w-full h-full p-4">
+            <h3 className="font-bold text-text">Categories</h3>
+        </div>
+    )
+}
