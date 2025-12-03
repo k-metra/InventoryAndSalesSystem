900e425925cfacfa0a26ba4a33dc7a298c5c9027
k-metra <metrakurt@gmail.com>
2025-12-03 11:19:37 +0800

    refactor: remove unnecessary commented lines

--- frontend/src/utils/fetchDashboardResources.ts
+++ frontend/src/utils/fetchDashboardResources.ts
@@ -23,25 +23,4 @@ const fetchDashboardResources = async () => {
     };
 }
 
-
-// const fetchDashboardResources = new Promise<dashboardResources>(async (resolve, _) => {
-//     console.log("Fetching dashboard resources...");
-//     const { totalProducts, totalInventoryValue } = 
-//         await api.get('/products/all')
-//         .then(res => res.data)
-//         .catch((err) => {
-//             console.log("Error fetching product count", err);
-//             return 0;
-//         });
-
-//     resolve(
-//         {
-//             productCount: totalProducts,
-//             totalInventoryValue: formatCurrency(totalInventoryValue),
-//             totalSales: 127,
-//             totalCustomers: 12
-//         }
-//     );
-// });
-
 export default fetchDashboardResources;
