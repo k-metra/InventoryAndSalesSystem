9e3531a60ef725d1d48dbf1a2b2b3b969f5f6a0f
k-metra <metrakurt@gmail.com>
2025-12-04 11:55:12 +0800

    fix(useSuppliers): add search params to useSuppliers query

--- frontend/src/queries/suppliers/useSuppliers.ts
+++ frontend/src/queries/suppliers/useSuppliers.ts
@@ -1,11 +1,16 @@
 import api from '../../axios/api';
 import { useQuery } from '@tanstack/react-query';
 
-export default function useSuppliers() {
+export default function useSuppliers(search: string = '') {
     return useQuery({
-        queryKey: ['suppliers'],
-        queryFn: async () => {
-            const res = await api.get('/suppliers');
+        queryKey: ['suppliers', search],
+        queryFn: async ({ queryKey }) => {
+            const [_key, search] = queryKey;
+
+            const params = new URLSearchParams();
+            if (search && search.length > 0) params.set('search', search);
+
+            const res = await api.get(`/suppliers?${params.toString()}`);
             return res.data;
         },
     });
