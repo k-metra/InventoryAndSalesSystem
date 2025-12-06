d3a6cbd38a100e4cac80371873f5592fdef43ea9
k-metra <metrakurt@gmail.com>
2025-12-06 16:02:27 +0800

    feat(customersPage): implement initial error and loading pages for customersPage

--- frontend/src/pages/dashboard/customersPage.tsx
+++ frontend/src/pages/dashboard/customersPage.tsx
@@ -1,3 +1,26 @@
+import { FaSpinner } from "react-icons/fa";
+import useCustomers from "../../queries/customers/useCustomers";
+
 export default function CustomersPage() {
-    return <div>Customers Page</div>;
+
+    const { 
+        data: Customers, 
+        isLoading: isCustomersLoading, 
+        isError: isCustomersError }
+         = useCustomers();
+    
+    if (isCustomersError) return <div className="w-full h-full p-6 text-lg text-red-500">Ran into an error loading customers.</div>;
+
+    if (isCustomersLoading) return (
+        <div className="w-full h-full p-6 flex items-center justify-center text-md text-text">
+            Loading Customers...
+            <FaSpinner className="inline-block ml-2 animate-spin" />
+        </div>
+    )
+
+    return (
+        <div className="w-full h-full p-4">
+            <h4 className="font-bold mb-4 text-text">All Customers</h4>
+        </div>
+    )
 }
