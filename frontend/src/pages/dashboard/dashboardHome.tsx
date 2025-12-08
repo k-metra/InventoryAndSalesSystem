59b77897cec5e9fcdcd5a7cd3e93bf457298f20d
k-metra <metrakurt@gmail.com>
2025-12-08 22:22:00 +0800

    feat(Dashboard): add highlighted alert for displaying low stock count with increased visibility
    Appears right beside the dashboard title

--- frontend/src/pages/dashboard/dashboardHome.tsx
+++ frontend/src/pages/dashboard/dashboardHome.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import ChartCard from "../../components/chartCard";
 import InventoryTrends from "../../components/charts/inventoryTrends";
 import SalesOverTime from "../../components/charts/salesOverTime";
@@ -13,7 +14,7 @@ import { AiOutlineReload } from "react-icons/ai";
 
 export default function DashboardHome() {
     // const queryClient = useQueryClient();
-    const [shouldRefetch, setShouldRefetch] = useState(true);
+    const navigate = useNavigate();
 
     const { data, isPending, isError, refetch } = useDashboard();
 
@@ -34,7 +35,20 @@ export default function DashboardHome() {
     return (
         <div id="container" className="custom-scrollbar p-4 overflow-y-auto overflow-x-hidden w-full h-full flex flex-col gap-4">
             <div className="flex justify-between items-center w-full">
-                <h3 className="font-bold text-text">Dashboard</h3>
+                <div className="flex justify-center items-center gap-4">
+                    <h3 className="font-bold text-text">Dashboard</h3>
+                    {lowStockCount > 0 && (
+                        <span 
+                            onClick={
+                                () => {
+                                    navigate('/dashboard/products?sortBy=Lower+Stock');
+                                }
+                            }
+                            className="cursor-pointer  text-red-500 border border-red-500 bg-red-500/10 font-semibold text-[12px] px-2 py-1 rounded-full shadow-[0px_0px_20px_5px_rgba(255,0,0,0.1)] hover:shadow-[0px_0px_20px_8px_rgba(255,0,0,0.25)] hover:bg-red-500/20 hover:font-bold transition-shadow-colors duration-500 ease-out">
+                            {lowStockCount} items are low on stock!
+                        </span>
+                    )}
+                </div>
 
                 <label className="relative group">
                     <button onClick={() => refetch()} className="to-blue-600 from-blue-500 bg-linear-to-r hover:to-blue-700 hover:from-blue-600 text-white px-4 pr-9 py-2 rounded-md transition-colors duration-300 cursor-pointer">
@@ -46,7 +60,7 @@ export default function DashboardHome() {
                     />
                 </label>
             </div>
-            <div className="grid grid-cols-4 gap-4 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-4 xl:grid-cols-5 gap-4 w-full">
                 <KpiCard title="Total Products" value={totalProducts} subtitle={"Total amount in inventory"} />
                 <KpiCard title="Total Sales" value={totalSalesToday} subtitle={"This day"} />
                 <KpiCard title="Total Sales" value={totalSalesThisMonth} subtitle={"This month"} />
