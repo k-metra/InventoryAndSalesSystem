b27d83209f47b481f112da458516f890f6a1add1
k-metra <metrakurt@gmail.com>
2025-12-12 19:04:18 +0800

    add search as query key to useProducts query

--- frontend/src/queries/products/useProducts.ts
+++ frontend/src/queries/products/useProducts.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import api from '../../axios/api';
 
 
-export default function useProducts() {
+export default function useProducts(searchQuery: string) {
     return useQuery({
-        queryKey: ['products'],
-        queryFn: async () => {
+        queryKey: ['products', searchQuery],
+        queryFn: async ({ queryKey }) => {
+            const [_key, query] = queryKey;
             return await api.get('/products').then(res => res.data);
         }
     });
