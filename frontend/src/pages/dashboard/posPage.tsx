d6dd7389e41fcd54a9d885c5e73f9346fd518547
k-metra <metrakurt@gmail.com>
2025-12-13 09:02:51 +0800

    add basic dropdown animation for sort function in pos page

--- frontend/src/pages/dashboard/posPage.tsx
+++ frontend/src/pages/dashboard/posPage.tsx
@@ -3,7 +3,10 @@ import { TiShoppingCart } from "react-icons/ti";
 import SearchBar from "../../components/searchBar";
 import useProducts from "../../queries/products/useProducts";
 import { useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { RiArrowDropDownFill } from "react-icons/ri";
 
 export default function POSPage() {
     
@@ -15,12 +18,27 @@ export default function POSPage() {
     const [supplier, setSupplier] = useState<string | null>(searchParams.get("supplier"));
     const [category, setCategory] = useState<string | null>(searchParams.get("category"));
 
+    const [sortOpen, setSortOpen] = useState(false);
+
     const {
         data: products,
         isPending: isProductsLoading,
         isError: isProductError,
         refetch: refetchProducts
     } = useProducts(activeSearch || "");
+
+    const sortingOptions = useMemo(() => {
+        return [
+            'A-Z',
+            'Z-A',
+            'Greater Price',
+            'Lower Price',
+            'Greater Stock',
+            'Lower Stock',
+            'Greater Cost',
+            'Lower Cost',
+        ];
+    }, []);
 
     return (
         <div className="p-4 grid grid-cols-1 md:grid-cols-2 gap-2">
@@ -39,6 +57,29 @@ export default function POSPage() {
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
                     value={search}
                 />
+                <div className="flex gap-1 my-2">
+                    <div className="relative inline-block">
+                        <button
+                            onClick={() => setSortOpen(prev => !prev)}
+                            className="cursor-pointer hover:bg-black/10 transition-colors duration-300 px-2 p-1 rounded-full text-[12px] bg-secondary border border-black/30 text-text flex items-center justify-center"
+                        >
+                            Sort by
+                            <RiArrowDropDownFill size={20} className="inline-block" />
+                        </button>
+                        <div className={`p-1 flex flex-col gap-2 overflow-y-auto absolute top-full left-0 mt-1 w-max bg-background border border-black/25 rounded-md shadow-lg z-10 ${sortOpen ? 'animate-dropdown-open' : 'animate-dropdown-close'}`}>
+                            {sortingOptions.map((option, idx) => (
+                                <button 
+                                    key={idx}
+                                    className={`block p-1 hover:bg-black/20 transition-colors duration-300 text-center text-sm ${option === sort && 'font-semibold border-l-4 border-primary'}`}
+                                >
+                                    {option}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                
                 <hr className="border-t border-black/25 my-4" />
 
                 {isProductsLoading ? (
