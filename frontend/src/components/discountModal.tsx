ed2c44a653ddd5481d6b9ccdc0c0ff4b6dd09130
k-metra <metrakurt@gmail.com>
2025-12-21 13:51:53 +0800

    initialize discount modal file

---
+++ frontend/src/components/discountModal.tsx
@@ -0,0 +1,9 @@
+export default function DiscountModal() {
+    return (
+        <div className="fixed w-screen h-screen left-0 top-0 bg-black/50 flex justify-center items-center">
+            <div className="bg-white p-6 rounded-lg w-96">
+                <h2 className="text-xl font-semibold mb-4">Apply Discount</h2>
+            </div>
+        </div>
+    )
+}
