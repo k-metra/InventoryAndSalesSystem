099c10dcd0878455812a10b381f4d715ab83e433
k-metra <metrakurt@gmail.com>
2025-12-27 21:54:01 +0800

    fix(Toasts): increase z-index to be above black overlay by one

--- frontend/src/contexts/ToastContext.tsx
+++ frontend/src/contexts/ToastContext.tsx
@@ -40,7 +40,7 @@ const ToastProvider = ({ children } : {children: React.ReactNode }) => {
     return <ToastContext.Provider value={{ addToast }}>
         { children }
 
-        <div className="fixed bottom-4 right-4 flex flex-col gap-2 z-2000">
+        <div className="fixed bottom-4 right-4 flex flex-col gap-2 z-10000">
             {toasts.map((toast) => (
                 <Toast 
                     key={toast.id} 
