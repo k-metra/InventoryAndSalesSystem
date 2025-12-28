6d1a503f2beb290c657392ba180b2b8ee20f5ab8
k-metra <metrakurt@gmail.com>
2025-12-28 22:07:58 +0800

    refactor: move SalesBody object type into types/requests.ts

--- frontend/src/components/checkoutModal.tsx
+++ frontend/src/components/checkoutModal.tsx
@@ -23,18 +23,6 @@ type CheckoutModalProps = {
     close: () => void;
 }
 
-type SaleBody = {
-    customer_id: number | null;
-    payment_method: "cash" | "card" | "mobile" | "",
-    subtotal: number;
-    amount_received: number;
-    discount_amount: number;
-    vat_rate: number;
-    vat_amount: number;
-    total: number;
-    items: Item[];
-}
-
 export default function CheckoutModal( {
     subtotal,
     vat_amount,
